ed3e7c6034e04cb6f99b2c7313e8442c157fac1f
zzanggoon8 <zzanggoon8@gmail.com>
2022-01-14 21:52:35 +0900

    Security URL

--- src/main/java/me/ssu/springsecurity/configs/SecurityConfig.java
+++ src/main/java/me/ssu/springsecurity/configs/SecurityConfig.java
@@ -2,12 +2,14 @@ package me.ssu.springsecurity.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     // TODO 시큐리티 기본 설정(URL)-1
     @Override
